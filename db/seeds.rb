8e8f0be883be821aca6810ed0b97aa0a040427f6
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-09 00:10:55 -0500

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Airport.create(Code: "ATL", City: "Atlanta")
+Airport.create(Code: "DFW", City: "Dallas & Fort Worth")
+Airport.create(Code: "DEN", City: "Denver")
+Airport.create(Code: "ORD", City: "Chicago")
+      
+a1 = Airport.find_or_create_by(Code: "ATL")
+a2 = Airport.find_or_create_by(Code: "DFW")
+a3 = Airport.find_or_create_by(Code: "DEN")
+a4 = Airport.find_or_create_by(Code: "ORD")
+Flight.create!( Date: Time.new(2023, 7, 9, 20,0,0).strftime("%d/%m/%Y"), Duration: 2, DepartureAirport: a1, DestinationAirport: a2 )
+Flight.create!(Date: Time.new(2023, 7, 10, 10,0,0).strftime("%d/%m/%Y"), Duration: 2, DepartureAirport: a3, DestinationAirport: a4 )
