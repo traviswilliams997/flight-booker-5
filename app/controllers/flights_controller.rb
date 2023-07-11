5f13bbf954094a4e57526f8208b3de1804711918
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-10 20:00:36 -0500

    Edit root page

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,8 @@ class FlightsController < ApplicationController
   # GET /flights or /flights.json
   def index
     @flights = Flight.all
+    @flight = Flight.new
+
   end
 
   # GET /flights/1 or /flights/1.json
