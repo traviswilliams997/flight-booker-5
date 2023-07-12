8228487462124db29a5d7a487a22bf859448ce1e
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-12 18:25:35 -0500

    Create booking controller and routes

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,14 @@
+class BookingsController < ApplicationController
+    def show 
+    
+    end 
+    
+    def new 
+
+    end
+    
+    def create 
+    end 
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :flights
   resources :airports
+  resources :bookings
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
