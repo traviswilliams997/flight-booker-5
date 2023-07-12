d639c6fadd8feea04aa58a6550aa7cb727c8217a
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-07-12 18:48:46 -0500

    Add  new booking functionality

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,11 +4,21 @@ class BookingsController < ApplicationController
     end 
     
     def new 
+        @booking = Booking.new
+        @flight = Flight.find(params[:flight_id])
+        @num_of_passengers = params[:num_of_passengers]
+        @booking.passengers.build
 
     end
     
     def create 
     end 
 
+    private
     
+    def booking_params
+      params.require(:booking).permit(:flight_id,
+                                      passengers_attributes: [:name, :email])
+      end
+
 end
